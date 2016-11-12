be4239ed1e93483f7e8e1356274026257407fc70
John Elash <elash@telus.net>
2016-11-11 16:45:05 -0800

    adding view and forms info - 1

---
+++ zendtutorial/module/Album/src/Form/AlbumForm.php
@@ -0,0 +1,47 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: John
+ * Date: 2016-11-11
+ * Time: 3:37 PM
+ */
+
+namespace Album\Form;
+
+use Zend\Form\Form;
+
+class AlbumForm extends Form
+{
+    public function __construct($name = null)
+    {
+        parent::__construct('album');
+
+        $this->add([
+            'name' => 'id',
+            'type' => 'hidden',
+        ]);
+        $this->add([
+            'name' => 'title',
+            'type' => 'text',
+            'options' => [
+                'label' => 'Title',
+            ],
+        ]);
+        $this->add([
+            'name' => 'artist',
+            'type' => 'text',
+            'options' => [
+                'label' => 'Artist',
+            ],
+        ]);
+        $this->add([
+            'name' => 'submit',
+            'type' => 'submit',
+            'attributes' => [
+                'value' => 'Go',
+                'id' => 'submitbutton',
+            ],
+        ]);
+    }
+}
